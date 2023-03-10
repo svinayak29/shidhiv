cca38d0522b64e8912341d208f30ff89f697796c
jaGdish <jagdish.shinde@functionup.org>
2023-03-10 15:04:07 +0530

    revision : useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  //  data management  -/> developers
+  //  ui management  -/> react 
+
+  //  HOW USESTATE WORKS ? 
+  //  you updates dta   (DONE)
+  //  tell the react taht data is updated  (Done)
+  //   react will compare the data with initial data 10 + 11 , 12 ()dONE
+  //  if it dound different , it will update the ui  
+
+   //const [readData, updateData] = useState(initialValue)
+  const [count , setCount] = useState(10)
+  const [name , setName] = useState('jaGdish')
+  // let count = 0
+  let newName ;
+
+
+
+  function handleClickButton () {
+    // count = count + 1
+
+    if(count === 20) {
+      alert('limite reached !!')
+      return
+    }
+    setCount(count+1)
+
+
+    //  
+    // alert(3535)
+    // const newValue = count + 10
+    // initail not matched with new value , then only it updateas the data
+    // setCount(count+0)
+    // 10 + 0 => 10 === 10 
+    // count = count + 1
+    // console.log(count)
+  }
+
+  //  collect the name from input
+  //  useState ()
+  //  new value from step1 , update it using 
+  
+
+  function handleChnageNmaae (event) {
+    console.log(event.target.value)
+    newName = event.target.value
+  }
+
+
+
+  function changeName  () {
+    setName(newName)
+    // I WILL UPDATE THE NAME HERE IN USETATE
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div>
+        <h2>{count}</h2>
+        <button onClick={handleClickButton}>Click me !!</button>
+      </div>
+
+      
+      <div>
+        <h1>{name}</h1>
+        <input onChange={handleChnageNmaae}/>
+        <button onClick={changeName}>ChangeName</button>
+      </div>
+  
     </div>
   );
 }
